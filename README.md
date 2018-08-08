d59e4f84feae66e12e4224476b1dbb452d5b8d15
bonbonpa <sphinspf@gmail.com>
2018-08-08 14:19:32 +0700

    init all

---
+++ README.md
@@ -0,0 +1,22 @@
+# Express
+
+## Request object
+
+- query
+    - req.query
+- params
+    - req.params
+- body
+    - req.body
+
+## Response object
+
+- send
+- status
+- redirect
+
+## MVC
+
+- Model
+- View
+- Controller
---
+++ app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+
+var app = express();
+
+// add new 
+var fs = require('file-system');
+
+// Include controller
+fs.readdirSync('controllers').forEach(function(file) {
+  if(file.substr(-3) == '.js'){
+    const route = require('./controllers/' + file)
+    route.controller(app)
+  }
+})
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
+// app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(4444,function() {
+  console.log("listening on 4444");
+})
+
+module.exports = app;

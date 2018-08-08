e327d2766d1550baaa089d8d0aefaa1acf046921
bonbonpa <sphinspf@gmail.com>
2018-08-08 15:18:53 +0700

    update users description

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,8 @@ module.exports.controller = (app) => {
     //get users page
     app.get('/users', (req, res) => {
         res.render('users', {
-            title: 'Users'
+            title: 'Users',
+            description: 'This is the description of all the users'
         })
     })
 }
